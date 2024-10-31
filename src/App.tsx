69322dc93371b4f6894584d5e4a1d7afd0ce5efb
v4s1n <vasin.oleksii@gmail.com>
2024-10-31 23:33:49 +0100

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/header/Header";
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="title-bar">
+      <div className="title-bar-text">TaskHuYask</div>
+      <div className="title-bar-controls">
+        <button aria-label="Close"></button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
